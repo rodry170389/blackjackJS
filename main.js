7065be441811bace8119cc10ee1a861068a4eea3
Rodrigo Fuentes <rodry170389@gmail.com>
2023-03-16 12:00:21 +0100

    Updates

--- main.js
+++
@@ -1,3 +0,0 @@
-import './src/blackjack';
-import './style.css';
-
--- src/blackjack/usecases/index.js
+++
@@ -1,6 +0,0 @@
-
-export { crearCartaHTML } from './crear-carta-html';
-export { crearDeck } from './crear-deck';
-export { pedirCarta } from './pedir-carta';
-export { turnoComputadora } from './turno-computadora';
-export { valorCarta } from './valor-carta';
